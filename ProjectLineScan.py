fd302269c1716f3bde3dd1bd4709a1617a458243
KingfernJohn <49335210+KingfernJohn@users.noreply.github.com>
2022-07-10 21:40:30 +0200

    Update ProjectLineScan.py

--- ProjectLineScan.py
+++ ProjectLineScan.py
@@ -1,27 +1,24 @@
 import sys, os, time
-
 while True:
     time.sleep(1)
     os.system('cls')
     every_line = int(0)
     print()
-
-    for path, subdirs, files in os.walk("C:/Users/Kingfern/Desktop/KAI-Cryp"):
+    for path, subdirs, files in os.walk("C:/Users/*username*/Desktop/*folder-name*"):
         for name in files:
             if path.endswith('__pycache__'):
                 break
             elif name == '.gitignore' or name == 'LICENSE' or name == 'CHANGELOG.txt' or name == 'manifest.in' or name == 'README.md' or name == 'README.txt':
                 break
-            elif name.endswith('.py') == False:  # 2 Lines Filter PYTHON only
-                break                            #
+            elif name.endswith('.py') == False:  # PYTHON-Code only
+                break
             else:
                 path = path.replace("\\", "/")
-                path = path[path.find('KAI-Cryp'):]
+                path = path[path.find('*folder-name*'):]
                 insges = path+'/'+name
                 insges = str(insges)
                 lines = sum(1 for line in open(insges, 'r'))
                 print(f"{name}     \t->\t{lines}")
                 lines = int(lines)
                 every_line += lines
-
     print(f"\n{every_line}\n")
